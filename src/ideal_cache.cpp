7ec817b2908d3d74f85000c0815eaa39be478e78
Yaroslav Kuznetsov <asklater@bk.ru>
2024-09-12 23:08:57 +0300

    added test-program of ideal cache

---
+++ src/ideal_cache.cpp
@@ -0,0 +1,39 @@
+#include "cache.hpp"
+#include <iostream>
+
+int slow_get_page(int key);
+
+using func_t = int(int);
+
+int main()
+{
+    size_t cache_size = 0, request_nums = 0, ideal_hits = 0;
+
+    std::cin >> cache_size >> request_nums;
+
+    int key = 0;
+    std::vector<int> request_list;
+
+    for (size_t i = 0; i < request_nums; i++)
+    {
+        std::cin >> key;
+        request_list.push_back(key);
+    }
+
+    caches::IdealCache<int, int(int)> ideal_cache{cache_size, request_list};
+    
+    for (auto it = request_list.begin(); it != request_list.end(); it = std::next(it))
+    {
+        if (ideal_cache.lookup_update(*it, slow_get_page))
+            ideal_hits++;
+    }
+
+    std::cout << ideal_hits << std::endl;
+
+    return 0;
+}
+
+int slow_get_page(int key)
+{
+    return key;
+}
