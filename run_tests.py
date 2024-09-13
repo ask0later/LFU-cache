28c34f754e1eb60a8a553af10dcb129ca7c93887
Yaroslav Kuznetsov <asklater@bk.ru>
2024-09-13 15:28:33 +0300

    added tests

---
+++ run_tests.py
@@ -0,0 +1,40 @@
+from subprocess import run, Popen, PIPE
+from sys import executable
+import sys
+
+cache = 	sys.argv[1]
+binary = 	cache + "_cache"
+test_dir = 	"test/" + cache + "_tests/"
+program = "./build/src/" + binary
+
+num_test = 1
+is_ok = True
+for i in range(1, 11):
+	str_data = test_dir
+	
+	str_data += str(i) + ".dat"
+	file_in = open(str_data, "r")
+	
+	str_ans = test_dir
+	str_ans += str(i) + ".dat.ans"
+
+	ans = ""
+	for i in open(str_ans):
+		ans = i
+	
+	result = run(program, capture_output = True, encoding='cp866', stdin=file_in)
+	print("Test: " + str(num_test).strip())
+	print("Result: " + str(result.stdout).strip())
+	
+	is_ok &= (result.stdout == ans)
+	if result.stdout == ans:
+		print("OK")
+	else:
+		print("ERROR, correct: " + str(ans).strip())
+	print("-------------------------------------------------")
+	num_test += 1
+
+if is_ok:
+	print("TESTS PASSED")
+else:
+	print("TESTS FAILED")
